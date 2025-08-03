af69cbeaa461ab54562307b87db26eb03c8ddfb0
Antlin23 <anton.jumkil@gmail.com>
2025-08-04 00:12:45 +0200

    Changed to this GB repo, removed random spawn point

---
+++ resources/[vehicles]/23xc90bs/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'HD X MoviaSL.'
+
+files {
+	'common/carvariations.meta',
+	'common/handling.meta',
+	'common/vehicles.meta',
+	'common/carcols.meta',
+	'els/23xc90bs.xml'
+}
+
+data_file 'CARCOLS_FILE' 'common/carcols.meta'
+data_file 'HANDLING_FILE' 'common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/carvariations.meta'
+
+is_els 'true'
